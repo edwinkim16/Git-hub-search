524dd805147e6574850f95386395ad8d942784df
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-29 14:36:53 +0300

    adding url and place it in gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"https://api.github.com/users/",
+  accessToken:"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"
 };
 
 /*
