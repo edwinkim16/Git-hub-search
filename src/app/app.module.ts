1cc2f43cf55d80e717bd9033be7558bc849d800f
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-29 15:06:17 +0300

    creating date pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
