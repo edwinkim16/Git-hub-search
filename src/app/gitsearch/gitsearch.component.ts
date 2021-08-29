ff6dab70b8293f3420144f9ceb482530688c4702
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-08-29 15:34:34 +0300

    importing classes/module needed in the component

--- src/app/gitsearch/gitsearch.component.ts
+++ src/app/gitsearch/gitsearch.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GitsearchServiceService } from '../gitsearch-service.service';
+import { HttpClient } from '@angular/common/http';
+import {environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-gitsearch',
